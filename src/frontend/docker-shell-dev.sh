2ce5b462ac9bf3a089279e539f515b7ed4de7187
Kamran Ahmed <k.ahmed@berkeley.edu>
2021-11-21 19:03:05 -0500

    Fix node modules volume in docker shell script

--- src/frontend/docker-shell-dev.sh
+++ src/frontend/docker-shell-dev.sh
@@ -16,5 +16,6 @@ docker build -t $IMAGE_NAME -f Dockerfile.dev .
 # --mount: Attach a filesystem mount to the container
 # -p: Publish a container's port(s) to the host (host_port: container_port) (source: https://dockerlabs.collabnix.com/intermediate/networking/ExposingContainerPort.html)
 docker run --rm --name $IMAGE_NAME -ti \
+-v /app/node_modules \
 --mount type=bind,source="$BASE_DIR",target=/app \
 -p 3000:3000 $IMAGE_NAME
