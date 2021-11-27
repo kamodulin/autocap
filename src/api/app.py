e6137243a54f983812ef9c27724a939e5459d647
Kamran Ahmed <k.ahmed@berkeley.edu>
2021-11-27 16:28:16 -0500

    Add gunicorn as production server

--- src/api/app.py
+++ src/api/app.py
@@ -4,6 +4,4 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-if __name__ == "__main__":
-    from views import *
-    app.run(host="0.0.0.0", port=5000, debug=True)
+from views import *
