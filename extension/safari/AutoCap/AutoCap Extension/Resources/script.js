720dee749046fc9257fdfcc1a923cc725b5bad61
Kamran Ahmed <k.ahmed@berkeley.edu>
2021-12-07 18:58:51 -0500

    Update browser script to process images sequentially

--- extension/safari/AutoCap/AutoCap Extension/Resources/script.js
+++ extension/safari/AutoCap/AutoCap Extension/Resources/script.js
@@ -1,15 +1,20 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+async function processPage() {
     var images = document.images;
     var imagesLength = images.length;
-    
+
     for (let i = 0; i < imagesLength; i++) {
         let image = images[i];
         if (!image.alt) {
-            srcToFile(images[i].src).then(file => {
+            const text = await srcToFile(images[i].src).then(file => {
                 process(file).then(res => {
+                    console.log(res);
                     image.alt = res;
                 });
             });
         }
     }
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    processPage();
 });
